48e05ed2c804fad27f910ca1057d036da4670a14
unknown <john.berg.96bits@gmail.com>
2019-11-21 07:15:16 +0000

    Basic expr eval!!

--- src/GP/Main.cpp
+++ src/GP/Main.cpp
@@ -12,16 +12,44 @@ enum OpCode: std::uint64_t
 
 
 template<typename ForwardIt>
-ForwardIt Eval(ForwardIt first, ForwardIt last)
+ForwardIt FindEndOfExpr(ForwardIt first, ForwardIt last)
 {
     // WIP
     switch (*(first++))
     {
     case OpCode::LoadConst:
-        context.Terminal(*first);
-        return first;
+        return ++first;
     case OpCode::Add:
-        return first;
+    case OpCode::Mul:
+        auto i = FindEndOfExpr(first, last);
+        i = FindEndOfExpr(i, last);
+        return i;
+    }
+}
+
+template<typename ForwardIt>
+double EvalExpr(ForwardIt first, ForwardIt last) 
+{
+    switch (*(first++))
+    {
+    case OpCode::LoadConst:
+        double bits;
+        std::memcpy(&bits, &*first, sizeof(double));
+        return bits;
+    case OpCode::Add:
+        {
+            auto i = FindEndOfExpr(first, last);
+            if (i == last)
+                return 0.0;
+            return EvalExpr(first, i) + EvalExpr(i, last);
+        }
+    case OpCode::Mul:
+        {
+            auto i = FindEndOfExpr(first, last);
+            if (i == last)
+                return 0.0;
+            return EvalExpr(first, i) + EvalExpr(i, last);
+        }
     }
 }
 
@@ -42,8 +70,14 @@ OutputIt InsertLoadConst(OutputIt outIt, double constant)
 
 int main()
 {
+    code.push_back(Add);
     auto i = std::back_inserter(code);
     i = InsertLoadConst(i, 100);
+    i = InsertLoadConst(i, 200);
 
-    std::cout << code.size();
+    std::cout << code.size() << '\n';
+
+    auto i1 = FindEndOfExpr(code.begin(), code.end());
+    std::cout << (i1 == code.end()) << '\n';
+    std::cout << EvalExpr(code.begin(), code.end());
 }
