513444bc725eff9f624a375d7b6834a9afaecde2
unknown <john.berg.96bits@gmail.com>
2019-12-03 22:08:45 +0000

    Almoste there

---
+++ include/CS3910/Core.h
@@ -0,0 +1,47 @@
+#ifndef CS3910__CORE_H_
+#define CS3910__CORE_H_
+
+#include "Pallets.h"
+#include <exception>
+#include <ostream>
+
+struct Dataset
+{
+    PalletData trainingData;
+    PalletData testingData;
+};
+
+class DataSizeMismatch final: public std::exception
+{
+public:
+    char const* what() const noexcept final
+    {
+        return "The training data and the testing data are incompatible";
+    }
+};
+
+Dataset ReadPalletData(int argc, char const** argv, std::ostream& outs)
+{
+    auto trainDataFile = "sample/cwk_train.csv";
+    auto testDataFile = "sample/cwk_test.csv";
+
+    if (argc < 3)
+    {
+        outs << "Too few arguments, 2 arguments are required: "
+            << "Expected a file containing training data "
+            << "and a file containing test data\n"
+            << "Using the default files "
+            << trainDataFile << " and " << testDataFile
+            << '\n';
+    }
+    else
+    {
+        trainDataFile = argv[1];
+        testDataFile = argv[2];
+    }
+
+    Dataset data{PalletData{trainDataFile}, PalletData{testDataFile}};
+    if(data.trainingData.DataCount() != data.testingData.DataCount())
+        throw DataSizeMismatch{};
+}
+#endif // !CS3910__CORE_H_
